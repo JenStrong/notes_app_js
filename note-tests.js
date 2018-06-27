1f6fd8e0479ce1bcb3455a57f51891322eef231f
Jen Strong <jenniferastrong@gmail.com>
2018-06-27 14:39:13 +0100

    Changed List-Model to hold array of notes, not note texts

--- note-tests.js
+++ note-tests.js
@@ -9,13 +9,16 @@ testNoteText();
 function testNoteList() {
   var noteList = new NoteList();
   noteList.addNote("My favourite language is JavaScript");
-  assert.isTrue(noteList.noteArray[0] === "My favourite language is JavaScript");
+  assert.isTrue(noteList.noteArray[0].returnNoteText() === "My favourite language is JavaScript");
 };
 
 testNoteList();
 
 function testNoteListView() {
-  var noteListView = new NoteListView(["First note", "Second note"]);
+  var noteList = new NoteList();
+  noteList.addNote("First note");
+  noteList.addNote("Second note");
+  var noteListView = new NoteListView(noteList);
   assert.isTrue(noteListView.returnHTML() === "<ul><li><div>First note</div></li><li><div>Second note</div></li></ul>");
 };
 
