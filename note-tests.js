b75679cc19755d5e8ccf05a6581d8781b4f4b1f4
Jen Strong <jenniferastrong@gmail.com>
2018-06-28 17:32:07 +0100

    moved note-control test into group test file

--- note-tests.js
+++ note-tests.js
@@ -23,3 +23,19 @@ function testNoteListView() {
 };
 
   testNoteListView();
+
+function testNoteController() {
+  function NoteListDouble() {};
+  NoteListDouble.prototype = {
+    addNote: function() {}
+  }
+  var notelistdouble = new NoteListDouble;
+  var notecontroller = new NoteController(notelistdouble);
+  notecontroller.noteListView.returnHTML = function () {
+    return "<ul><li><div>Favourite food: pesto</div></li></ul>";
+  }
+  notecontroller.createInnerHTML('app');
+  assert.isTrue(app.innerHTML === "<ul><li><div>Favourite food: pesto</div></li></ul>")
+};
+
+  testNoteController();
